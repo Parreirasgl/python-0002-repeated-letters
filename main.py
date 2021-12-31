81e6274980629bcc41c8d90a37164c93a05d3560
Guilherme Parreiras <parreirasgl@hotmail.com>
2021-12-30 23:11:11 -0300

    Repeated syllable - middle

--- main.py
+++ main.py
@@ -18,15 +18,20 @@ while counter < length:
         length -= 1
     else:
         for i_index, i_letter in enumerate(word2):
-            if word1[counter] == i_letter:
-                word2[]
-
-
-
-        word2 += word1[counter]
-
+            if i_letter == word1[counter]:
+                for j_index, j_letter in enumerate(word2, start=i_index):
+                    counter2 = 0
+                    list_index = []
+                    if j_letter == word1[counter + counter2]:
+                        list_index = counter + counter2
+                        counter2 += 1
+                        if counter2 == (len(word2) - i_index):
+                            for k in list_index:
+                                word1 = word1[:k] + word1[(k + 1):]
+                    else:
+                        word2 += word1[counter]
+                        break
     counter += 1
-
 
 print(word1)
 print(word2)
