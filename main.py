21dbcd060e6f028ee8d9981be244a1b9844dec3c
Guilherme Parreiras <parreirasgl@hotmail.com>
2021-12-30 21:35:46 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Exercise I took from my own head: eliminate repeated letters from words.
+# Exercício que eu tirei da minha cabeça: elimine letras repetidas de palavras.
+
+word1 = "woordddds"
+word2 = ""
+
+length = len(word1)
+counter = 0
+while counter < length:
+    print(counter)
+    print(word1)
+
+    if counter == 0:
+        word2 += word1[0]
+    elif word1[counter] == word2[len(word2)-1]:
+        word1 = word1[:counter] + word1[(counter + 1):]
+        counter -= 1
+        length -= 1
+    else:
+        word2 += word1[counter]
+
+    counter += 1
+
+
+print(word1)
+print(word2)
+
