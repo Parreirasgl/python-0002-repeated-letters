dbb1eb0a00c8b5650fb400dc5bcb43938cd711da
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-01 23:32:07 -0300

    Comment on while loop

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ word2 = ""
 
 length = len(word1)
 counter = 0
+
+# Create a while loop that runs a number of times equal to the word length.
+# The word length will change during the loop due to exclusion of lettersfrom the word.
+# Thus, there must be correction in the variable counter when letters are excluded.
+# Criar um lopp do tipo while que rode um número de vezes igual ao tamanho da palavra.
+# O tamanho da palavra vai mudar durante o while loop, pelo fato de se excluir
+# letras da palavra.
+# Desse modo, deve haver correção no na variável counter quando letras são excluídas.
 while counter < length:
     print(counter)
     print(word1)
+    print(word2)
 
     if counter == 0:
         word2 += word1[0]
@@ -29,8 +38,13 @@ while counter < length:
                             for k in list_index:
                                 word1 = word1[:k] + word1[(k + 1):]
                     else:
-                        word2 += word1[counter]
+                        counter2 = 100000
                         break
+                if counter2 == 100000:
+                    word2 += word1[counter]
+                    break
+            else:
+                word2 += word1[counter]
     counter += 1
 
 print(word1)
