7d7c336069f2023e8829e63cc4cf414422799cf2
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-01 23:42:31 -0300

    Comment on firt letter

--- main.py
+++ main.py
@@ -8,23 +8,27 @@ length = len(word1)
 counter = 0
 
 # Create a while loop that runs a number of times equal to the word length.
-# The word length will change during the loop due to exclusion of lettersfrom the word.
+# The word length will change during the loop due to exclusion of letters from the word.
 # Thus, there must be correction in the variable counter when letters are excluded.
 # Criar um lopp do tipo while que rode um número de vezes igual ao tamanho da palavra.
 # O tamanho da palavra vai mudar durante o while loop, pelo fato de se excluir
 # letras da palavra.
-# Desse modo, deve haver correção no na variável counter quando letras são excluídas.
+# Desse modo, deve haver correção na variável counter quando letras são excluídas.
 while counter < length:
     print(counter)
     print(word1)
     print(word2)
 
+# Add the first letter of word1 to word2.
+# Adicionar a primeira letra de word1 em word2.
     if counter == 0:
         word2 += word1[0]
+
     elif word1[counter] == word2[len(word2)-1]:
         word1 = word1[:counter] + word1[(counter + 1):]
         counter -= 1
         length -= 1
+
     else:
         for i_index, i_letter in enumerate(word2):
             if i_letter == word1[counter]:
