8d9b3b4e8c4ecc3395c950c2cb6e03c4f78ee54a
Guilherme Parreiras <parreirasgl@hotmail.com>
2022-01-02 17:07:37 -0300

    Repeated syllable - middle4 (prints)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # An exercise I took from my own head: eliminate repeated letters from words.
 # Um exercício que eu tirei da minha cabeça: elimine letras repetidas de palavras.
 
-word1 = "akaekarkart"
+word1 = "akaeekarkarty"
 word2 = ""
 
 length = len(word1)
 counter = 0
 equal_letter_inside_word2 = False
 equal_letters = 0
+list_index = []
 
 # Create a while loop that runs a number of times equal to the word length.
 # The word length will change during the loop due to exclusion of letters from the word.
@@ -17,9 +18,9 @@ equal_letters = 0
 # letras da palavra.
 # Desse modo, deve haver correção na variável counter quando letras são excluídas.
 while counter < length:
-    print(f"counter {counter}")
-    print(f"word1 {word1}")
-    print(f"word2 {word2}")
+    print(f"counter: {counter}")
+    print(f"word1: {word1}")
+    print(f"word2: {word2}")
     print("")
 
 # Add the first letter of word1 to word2.
@@ -34,8 +35,8 @@ while counter < length:
 # Se a letra for igual, apagar tal letra a word1, e corrigir as variáveis counter e length.
     elif word1[counter] == word2[len(word2)-1]:
         word1 = word1[:counter] + word1[(counter + 1):]
-        counter -= 1
         length -= 1
+        print("letra da vez de w1 igual última letra de w2")
 
 # If the letter is different, do a for loop passing through each letter of word2.
 # Se a letra for diferente, fazer um for loop passando por cada letra da word2.
@@ -47,19 +48,24 @@ while counter < length:
             if i_letter == word1[counter]:
                 equal_letter_inside_word2 = True
                 for j_index, j_letter in enumerate(word2, start=i_index):
-                    list_index = []
+                    print(f"j_index: {j_index}")
+                    print(f"j_letter: {j_letter}")
 
 # Durante o loop em word2, avaliar se há letra igual em word1, a partir da letra da vez.
 # Se houver, adicionar o index da letra em list_index.
                     if j_letter == word1[counter + equal_letters]:
                         list_index.append(counter + equal_letters)
                         equal_letters += 1
-                        print(list_index)
+                        print(f"list_index: {list_index}")
+                        print(f"equal_letters: {equal_letters}")
+                        print("")
 
-# Qualquer das letras em word1 for diferente, continuar o loop inicial através de word2.
+# Se qualquer das letras em word1 for diferente, continuar o loop inicial através de word2.
                     else:
+                        print("tem w2 dif w1")
                         equal_letters = 0
                         equal_letter_inside_word2 = False
+                        list_index = []
                         break
 
 # Se o número de letras em list_index for igual ao número de letra analisadas...
@@ -74,13 +80,18 @@ while counter < length:
 
         if equal_letters == 100000:
             word2 += word1[counter]
-            equal_letters = 0
+            print("saída1")
             break
 
         if equal_letter_inside_word2 == False:
             word2 += word1[counter]
             counter += 1
+            print("saída2")
 
+    # equal_letters = 0
+    # list_index = []
     equal_letter_inside_word2 = False
+    print("saída3")
+
 
 
